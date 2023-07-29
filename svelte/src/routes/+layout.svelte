1514b40b74f626f54fc92cea8cdd3a06695600d7
Sinkira <sinkirayt@gmail.com>
2023-07-29 15:43:28 +0400

    oyes

---
+++ svelte/src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Navbar from "./Navbar.svelte";
+</script>
+
+<Navbar />
+
+<div id="centerDiv">
+	<slot />
+</div>
+
+<div id="thirdDiv" />
+
+<style>
+	@media (max-width: 850px) {
+		#centerDiv {
+			background-color: rgba(150, 0, 214, 0.9);
+			width: 99.5%;
+			margin: auto;
+			border-radius: 27px;
+			border: 1px solid #a8f3fff2;
+			box-shadow: 0 0 10px #a8f3fff2;
+		}
+	}
+	@media (min-width: 850px) {
+		#centerDiv {
+			background-color: rgba(150, 0, 214, 0.9);
+			width: 85%;
+			margin: auto;
+			border-radius: 27px;
+			border: 1px solid #a8f3fff2;
+			box-shadow: 0 0 10px #a8f3fff2;
+		}
+	}
+
+	#thirdDiv {
+		min-height: 6vw;
+		padding: 1px;
+	}
+</style>
